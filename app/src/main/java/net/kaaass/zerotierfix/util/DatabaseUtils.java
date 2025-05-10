399290333157239fef1e01ee4be6779de38af639
Gmij <gmij@qq.com>
2025-05-10 09:54:12 +0000

    feat: 添加从数据库获取指定网络分配地址列表的方法

--- app/src/main/java/net/kaaass/zerotierfix/util/DatabaseUtils.java
+++ app/src/main/java/net/kaaass/zerotierfix/util/DatabaseUtils.java
@@ -1,8 +1,14 @@
 package net.kaaass.zerotierfix.util;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import net.kaaass.zerotierfix.model.AssignedAddress;
+import net.kaaass.zerotierfix.model.AssignedAddressDao;
+import net.kaaass.zerotierfix.model.DaoSession;
 
 /**
  * 数据库访问工具类
@@ -17,5 +23,26 @@ public class DatabaseUtils {
         readWriteLock = reentrantReadWriteLock;
         writeLock = reentrantReadWriteLock.writeLock();
         readLock = reentrantReadWriteLock.readLock();
+    }
+    
+    /**
+     * 从数据库中获取指定网络的分配地址列表
+     * 
+     * @param daoSession 数据访问会话
+     * @param networkId 网络ID
+     * @return 地址列表
+     */
+    public static List<AssignedAddress> getAddressesForNetwork(DaoSession daoSession, Long networkId) {
+        if (daoSession == null || networkId == null) {
+            return new ArrayList<>();
+        }
+        
+        // 获取分配地址DAO
+        AssignedAddressDao addressDao = daoSession.getAssignedAddressDao();
+        
+        // 查询指定网络ID的地址列表
+        return addressDao.queryBuilder()
+                .where(AssignedAddressDao.Properties.NetworkId.eq(networkId))
+                .list();
     }
 }
